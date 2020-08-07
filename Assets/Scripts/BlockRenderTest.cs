df37bb9905f690dd7018a927e54a12805d72496f
Trevor Akiyama <trevor.akiyama@gmail.com>
2020-08-06 23:08:37 -0700

    Updated the rendering to render basic cubes without needing to run Recalculate each time as well

--- Assets/Scripts/BlockRenderTest.cs
+++ Assets/Scripts/BlockRenderTest.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Schema;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Profiling;
+using Debug = UnityEngine.Debug;
 
 public class BlockRenderTest : MonoBehaviour
 {
@@ -10,51 +13,205 @@ public class BlockRenderTest : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
+    public static int chunkHeight = 64;
+    public static int chunkWidth = 64;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
 
-        // Create a Vertex array
+        UpdateMesh();
 
-        //List<Vector3> vertices = new List<Vector3>();
-        //List<int> triangles = new List<int>();
-        //List<Vector2> uvs = new List<Vector2>();
+        stopwatch.Stop();
 
-        //vertices.Add(new Vector3(0, 0, 0));
-        //vertices.Add(new Vector3(0, 1, 0));
-        //vertices.Add(new Vector3(1, 0, 0));
-        //vertices.Add(new Vector3(1, 1, 0));
+        Debug.LogFormat("Mesh Full Create time ={0}", stopwatch.Elapsed);
 
+    }
 
 
-        //triangles.Add(0);
-        //triangles.Add(1);
-        //triangles.Add(2);
-        //triangles.Add(2);
-        //triangles.Add(1);
-        //triangles.Add(3);
+
+    void generateVoxels()
+    {
+        // TODO:  Will require seeds for more interesting terrain
+
+        
 
 
-        //uvs.Add(new Vector2(0, 0));
-        //uvs.Add(new Vector2(0, 1));
-        //uvs.Add(new Vector2(1, 0));
-        //uvs.Add(new Vector2(1, 1));
 
 
-        VoxelFace frontFaceData = Voxel.getFrontFace();
-        VoxelFace topFaceData = Voxel.getTopFace();
+
+
+    }
+
+
+    void UpdateMesh()
+    {
+
+        // Walk through all the cubes and add to mesh
+
+        List<VoxelFace> faces = new List<VoxelFace>();
+
+
+
+
+        for (int x = 0; x < chunkWidth; x++)
+        {
+            for (int y = 0; y < chunkHeight; y++)
+            {
+                for (int z = 0; z < chunkWidth; z++)
+                {
+
+                    // for now assume it's a solid testblock
+
+
+                    // check all adjacent blocks for solidness
+                    // if solid then don't add the face
+                    // if not solid then add the face
+
+
+                    // Top
+                    if (y == chunkHeight - 1)
+                    {
+                        Profiler.BeginSample("Adding Top Face");
+                        faces.Add(Voxel.getTopFace(new Vector3(x, y, z)));
+                        Profiler.EndSample();
+                    }
+
+                    // Bottom
+                    if (y == 0)
+                    {
+                        faces.Add(Voxel.getBottomFace(new Vector3(x, y, z)));
+                    }
+
+                    // Right
+                    if (x == chunkWidth - 1)
+                    {
+                        faces.Add(Voxel.getRightFace(new Vector3(x, y, z)));
+                    }
+
+                    // Left
+                    if (x == 0)
+                    {
+                        faces.Add(Voxel.getLeftFace(new Vector3(x, y, z)));
+                    }
+
+                    // Back
+                    if (z == chunkWidth - 1)
+                    {
+                        faces.Add(Voxel.getBackFace(new Vector3(x, y, z)));
+                    }
+
+                    // Front
+                    if (z == 0)
+                    {
+                        faces.Add(Voxel.getFrontFace(new Vector3(x, y, z)));
+                    }
+                }
+            }
+
+        }
+
+
+
+        // Render faces
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        buildMesh(faces);
+
+        stopwatch.Stop();
+
+        Debug.LogFormat("Mesh Build time ={0}", stopwatch.Elapsed);
+
+
+
+    }
+
+
+    void buildMesh(List<VoxelFace> faces)
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<Vector3> normals = new List<Vector3>();
+
+        int faceCount = 0;
+        foreach (VoxelFace face in faces)
+        {
+
+            // copy the Vertex information
+            for (int i = 0; i < 4; i++)
+            {
+                vertices.Add(face.vertices[i]);
+                uvs.Add(face.uvs[i]);
+                normals.Add(face.normals[i]);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                triangles.Add(face.triangles[i] + faceCount * 4);
+            }
+
+            faceCount++;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.normals = normals.ToArray();
+
+        mesh.normals = normals.ToArray();
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        //mesh.RecalculateNormals();
+
+        stopwatch.Stop();
+
+        Debug.LogFormat("Normal Recalc time ={0}", stopwatch.Elapsed);
+
+
+
+ 
+
+
+        meshFilter.mesh = mesh;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void UpdateMeshCube()
+    {
+        VoxelFace frontFaceData = Voxel.getFrontFace(new Vector3(0,0,0));
+        VoxelFace topFaceData = Voxel.getTopFace(new Vector3(0, 0, 0));
 
 
         List<VoxelFace> faces = new List<VoxelFace>();
 
 
-        faces.Add(Voxel.getFrontFace());
-        faces.Add(Voxel.getTopFace());
-        faces.Add(Voxel.getLeftFace());
-        faces.Add(Voxel.getRightFace());
-        faces.Add(Voxel.getBottomFace());
-        faces.Add(Voxel.getBackFace());
+        faces.Add(Voxel.getFrontFace(new Vector3(0, 0, 0)));
+        faces.Add(Voxel.getTopFace(new Vector3(0, 0, 0)));
+        faces.Add(Voxel.getLeftFace(new Vector3(0, 0, 0)));
+        faces.Add(Voxel.getRightFace(new Vector3(0, 0, 0)));
+        faces.Add(Voxel.getBottomFace(new Vector3(0, 0, 0)));
+        faces.Add(Voxel.getBackFace(new Vector3(0, 0, 0)));
 
 
 
@@ -93,10 +250,10 @@ public class BlockRenderTest : MonoBehaviour
         mesh.triangles = triangles.ToArray();
         mesh.uv = uvs.ToArray();
 
-        //mesh.normals = normals.ToArray();
+        mesh.normals = normals.ToArray();
 
 
-        mesh.RecalculateNormals();
+        //mesh.RecalculateNormals();
 
 
 
@@ -130,6 +287,6 @@ public class BlockRenderTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMesh();
     }
 }
--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -1,30 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class Voxel
 {
 
 
+    enum FACE
+    {
+        TOP,
+        BOTTOM,
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+
+
+
+    static Vector2[] uvs = new Vector2[]
+        {
+        new Vector2(0, 0),
+        new Vector2(0, 1),
+        new Vector2(1, 0),
+        new Vector2(1,1)
+        };
+
+    static Vector3[] normals = new Vector3[]
+    {
+        new Vector3(0,1,0), // Top
+        new Vector3(0,-1,0),
+        new Vector3(0,0,0),
+        new Vector3(0,0,0),
+        new Vector3(0,0,0),
+        new Vector3(0,0,0),
+
+
+    };
+
+
+    static Vector3[] vertices = new Vector3[]
+    {
+        new Vector3(0,0,0),
+        new Vector3(1,0,0),
+        new Vector3(0,1,0),
+        new Vector3(1,1,0),
+        new Vector3(0,0,1),
+        new Vector3(1,0,1),
+        new Vector3(0,1,1),
+        new Vector3(1,1,1)
+    };
+
+
+
+
+
+
+    
+    /**
+     * Front
+     * Verts = 0,1,2,3
+     * Tris = 9,1,2,2,1,3
+     * norm =  0,0,-1
+     * 
+     * top
+     * verts = 2,6,3,7
+     * TriVerts = 0,1,2,2,1,3
+     * norm = 0,1,0
+     * 
+     * 
+     * Bottom
+     * verts = 0,4,1,5
+     * TriVerts = 0,2,1,1,2,3
+     * 
+
     
 
+    */
 
 
-
-    public static VoxelFace getFrontFace()
+    public static VoxelFace getFrontFace(Vector3 delta)
     {
 
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(0, 0, 0);
-        face.vertices[1] = new Vector3(0, 1, 0);
-        face.vertices[2] = new Vector3(1, 0, 0);
-        face.vertices[3] = new Vector3(1, 1, 0);
+        face.vertices[0] = new Vector3(0, 0, 0) + delta;
+        face.vertices[1] = new Vector3(0, 1, 0) + delta;
+        face.vertices[2] = new Vector3(1, 0, 0) + delta;
+        face.vertices[3] = new Vector3(1, 1, 0) + delta;
 
-        face.uvs[0] = new Vector2(0, 0);
-        face.uvs[1] = new Vector2(0, 1);
-        face.uvs[2] = new Vector2(1, 0);
-        face.uvs[3] = new Vector2(1, 1);
+        face.uvs[0] = uvs[0];
+        face.uvs[1] = uvs[1];
+        face.uvs[2] = uvs[2];
+        face.uvs[3] = uvs[3];
 
         Vector3 normal = new Vector3(0, 0, -1);
         face.normals[0] = normal;
@@ -39,18 +108,44 @@ public class Voxel
         face.triangles[4] = 1;
         face.triangles[5] = 3;
 
+
         return face;
     }
 
 
-    public static VoxelFace getTopFace()
+    public static VoxelFace getTopFace(Vector3 delta)
     {
+        //Profiler.BeginSample("Top Face Method2");
+
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(0, 1, 0);
-        face.vertices[1] = new Vector3(0, 1, 1);
-        face.vertices[2] = new Vector3(1, 1, 0);
-        face.vertices[3] = new Vector3(1, 1, 1);
+        face.vertices[0] = new Vector3(0, 1, 0) + delta;
+        face.vertices[1] = new Vector3(0, 1, 1) + delta;
+        face.vertices[2] = new Vector3(1, 1, 0) + delta;
+        face.vertices[3] = new Vector3(1, 1, 1) + delta;
+
+        face.vertices[0].x = vertices[2].x + delta.x;
+        face.vertices[0].y = vertices[2].y + delta.y;
+        face.vertices[0].z = vertices[2].z + delta.z;
+
+        face.vertices[1].x = vertices[6].x + delta.x;
+        face.vertices[1].y = vertices[6].y + delta.y;
+        face.vertices[1].z = vertices[6].z + delta.z;
+
+        face.vertices[2].x = vertices[3].x + delta.x;
+        face.vertices[2].y = vertices[3].y + delta.y;
+        face.vertices[2].z = vertices[3].z + delta.z;
+
+        face.vertices[3].x = vertices[7].x + delta.x;
+        face.vertices[3].y = vertices[7].y + delta.y;
+        face.vertices[3].z = vertices[7].z + delta.z;
+
+
+        //face.vertices[0] = vertices[2] + delta;
+        //face.vertices[1] = vertices[6] + delta;
+        //face.vertices[2] = vertices[3] + delta;
+        //face.vertices[3] = vertices[7] + delta;
+
 
         face.triangles[0] = 0;
         face.triangles[1] = 1;
@@ -59,30 +154,101 @@ public class Voxel
         face.triangles[4] = 1;
         face.triangles[5] = 3;
 
-        face.uvs[0] = new Vector2(0, 0);
-        face.uvs[1] = new Vector2(0, 1);
-        face.uvs[2] = new Vector2(1, 0);
-        face.uvs[3] = new Vector2(1, 1);
+        face.uvs[0] = uvs[0];
+        face.uvs[1] = uvs[1];
+        face.uvs[2] = uvs[2];
+        face.uvs[3] = uvs[3];
 
-        Vector3 normal = new Vector3(0, 1, 0);
-        face.normals[0] = normal;
-        face.normals[1] = normal;
-        face.normals[2] = normal;
-        face.normals[3] = normal;
+        //Vector3 normal = new Vector3(0, 1, 0);
+        face.normals[0] = normals[0];
+        face.normals[1] = normals[0];
+        face.normals[2] = normals[0];
+        face.normals[3] = normals[0];
+
+        //Profiler.EndSample();
 
         return face;
     }
 
 
-    public static VoxelFace getBottomFace()
+
+
+
+    public static VoxelFace getTopFace2(Vector3 delta)
+    {
+        //Profiler.BeginSample("Top Face Method2");
+
+        VoxelFace face = new VoxelFace();
+
+        face.vertices[0] = new Vector3(0, 1, 0) + delta;
+        face.vertices[1] = new Vector3(0, 1, 1) + delta;
+        face.vertices[2] = new Vector3(1, 1, 0) + delta;
+        face.vertices[3] = new Vector3(1, 1, 1) + delta;
+
+        face.vertices[0].x = vertices[2].x + delta.x;
+        face.vertices[0].y = vertices[2].y + delta.y;
+        face.vertices[0].z = vertices[2].z + delta.z;
+
+        face.vertices[1].x = vertices[6].x + delta.x;
+        face.vertices[1].y = vertices[6].y + delta.y;
+        face.vertices[1].z = vertices[6].z + delta.z;
+
+        face.vertices[2].x = vertices[3].x + delta.x;
+        face.vertices[2].y = vertices[3].y + delta.y;
+        face.vertices[2].z = vertices[3].z + delta.z;
+
+        face.vertices[3].x = vertices[7].x + delta.x;
+        face.vertices[3].y = vertices[7].y + delta.y;
+        face.vertices[3].z = vertices[7].z + delta.z;
+
+
+        //face.vertices[0] = vertices[2] + delta;
+        //face.vertices[1] = vertices[6] + delta;
+        //face.vertices[2] = vertices[3] + delta;
+        //face.vertices[3] = vertices[7] + delta;
+
+
+        face.triangles[0] = 0;
+        face.triangles[1] = 1;
+        face.triangles[2] = 2;
+        face.triangles[3] = 2;
+        face.triangles[4] = 1;
+        face.triangles[5] = 3;
+
+        face.uvs[0] = uvs[0];
+        face.uvs[1] = uvs[1];
+        face.uvs[2] = uvs[2];
+        face.uvs[3] = uvs[3];
+
+        //Vector3 normal = new Vector3(0, 1, 0);
+        face.normals[0] = normals[0];
+        face.normals[1] = normals[0];
+        face.normals[2] = normals[0];
+        face.normals[3] = normals[0];
+
+        //Profiler.EndSample();
+
+        return face;
+    }
+
+
+
+
+
+
+
+
+
+
+    public static VoxelFace getBottomFace(Vector3 delta)
     {
 
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(0, 0, 0);
-        face.vertices[1] = new Vector3(0, 0, 1);
-        face.vertices[2] = new Vector3(1, 0, 0);
-        face.vertices[3] = new Vector3(1, 0, 1);
+        face.vertices[0] = new Vector3(0, 0, 0) + delta;
+        face.vertices[1] = new Vector3(0, 0, 1) + delta;
+        face.vertices[2] = new Vector3(1, 0, 0) + delta;
+        face.vertices[3] = new Vector3(1, 0, 1) + delta;
 
         face.triangles[0] = 0;
         face.triangles[1] = 2;
@@ -107,21 +273,21 @@ public class Voxel
 
     }
 
-    public static VoxelFace getBackFace()
+    public static VoxelFace getBackFace(Vector3 delta)
     {
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(0, 0, 1);
-        face.vertices[1] = new Vector3(0, 1, 1);
-        face.vertices[2] = new Vector3(1, 0, 1);
-        face.vertices[3] = new Vector3(1, 1, 1);
+        face.vertices[0] = new Vector3(0, 0, 1) + delta;
+        face.vertices[1] = new Vector3(0, 1, 1) + delta;
+        face.vertices[2] = new Vector3(1, 0, 1) + delta;
+        face.vertices[3] = new Vector3(1, 1, 1) + delta;
 
         face.uvs[0] = new Vector2(0, 0);
         face.uvs[1] = new Vector2(0, 1);
         face.uvs[2] = new Vector2(1, 0);
         face.uvs[3] = new Vector2(1, 1);
 
-        Vector3 normal = new Vector3(0, 0, -1);
+        Vector3 normal = new Vector3(0, 0, 1);
         face.normals[0] = normal;
         face.normals[1] = normal;
         face.normals[2] = normal;
@@ -137,14 +303,14 @@ public class Voxel
         return face;
     }
 
-    public static VoxelFace getLeftFace()
+    public static VoxelFace getLeftFace(Vector3 delta)
     {
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(0, 0, 0);
-        face.vertices[1] = new Vector3(0, 1, 0);
-        face.vertices[2] = new Vector3(0, 0, 1);
-        face.vertices[3] = new Vector3(0, 1, 1);
+        face.vertices[0] = new Vector3(0, 0, 0) + delta;
+        face.vertices[1] = new Vector3(0, 1, 0) + delta;
+        face.vertices[2] = new Vector3(0, 0, 1) + delta;
+        face.vertices[3] = new Vector3(0, 1, 1) + delta;
 
         face.triangles[0] = 0;
         face.triangles[1] = 2;
@@ -158,7 +324,7 @@ public class Voxel
         face.uvs[2] = new Vector2(0, 0);
         face.uvs[3] = new Vector2(0, 1);
 
-        Vector3 normal = new Vector3(1, 0, 0);
+        Vector3 normal = new Vector3(-1, 0, 0);
         face.normals[0] = normal;
         face.normals[1] = normal;
         face.normals[2] = normal;
@@ -167,14 +333,14 @@ public class Voxel
         return face;
     }
 
-    public static VoxelFace getRightFace()
+    public static VoxelFace getRightFace(Vector3 delta)
     {
         VoxelFace face = new VoxelFace();
 
-        face.vertices[0] = new Vector3(1, 0, 0);
-        face.vertices[1] = new Vector3(1, 1, 0);
-        face.vertices[2] = new Vector3(1, 0, 1);
-        face.vertices[3] = new Vector3(1, 1, 1);
+        face.vertices[0] = new Vector3(1, 0, 0) + delta;
+        face.vertices[1] = new Vector3(1, 1, 0) + delta;
+        face.vertices[2] = new Vector3(1, 0, 1) + delta;
+        face.vertices[3] = new Vector3(1, 1, 1) + delta;
 
         face.triangles[0] = 0;
         face.triangles[1] = 1;
