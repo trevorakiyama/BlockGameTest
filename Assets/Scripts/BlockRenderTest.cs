516511028d0dd3cfea99dee57dda75ec12861eb9
Trevor Akiyama <trevor.akiyama@gmail.com>
2020-08-07 21:43:10 -0700

    Some more git cleanup

--- Assets/Scripts/BlockRenderTest.cs
+++ Assets/Scripts/BlockRenderTest.cs
@@ -13,8 +13,8 @@ public class BlockRenderTest : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
-    public static int chunkHeight = 64;
-    public static int chunkWidth = 64;
+    public static int chunkHeight = 256;
+    public static int chunkWidth = 16;
 
 
     void Start()
@@ -35,14 +35,6 @@ public class BlockRenderTest : MonoBehaviour
     void generateVoxels()
     {
         // TODO:  Will require seeds for more interesting terrain
-
-        
-
-
-
-
-
-
     }
 
 
@@ -71,37 +63,37 @@ public class BlockRenderTest : MonoBehaviour
                     // if not solid then add the face
 
 
-                    // Top
-                    if (y == chunkHeight - 1)
-                    {
-                        Profiler.BeginSample("Adding Top Face");
-                        faces.Add(Voxel.getTopFace(new Vector3(x, y, z)));
-                        Profiler.EndSample();
-                    }
+                    //// Top
+                    //if (y == chunkHeight - 1)
+                    //{
+                    //    Profiler.BeginSample("Adding Top Face");
+                    //    faces.Add(Voxel.getTopFace(new Vector3(x, y, z)));
+                    //    Profiler.EndSample();
+                    //}
 
-                    // Bottom
-                    if (y == 0)
-                    {
-                        faces.Add(Voxel.getBottomFace(new Vector3(x, y, z)));
-                    }
+                    //// Bottom
+                    //if (y == 0)
+                    //{
+                    //    faces.Add(Voxel.getBottomFace(new Vector3(x, y, z)));
+                    //}
 
-                    // Right
-                    if (x == chunkWidth - 1)
-                    {
-                        faces.Add(Voxel.getRightFace(new Vector3(x, y, z)));
-                    }
+                    //// Right
+                    //if (x == chunkWidth - 1)
+                    //{
+                    //    faces.Add(Voxel.getRightFace(new Vector3(x, y, z)));
+                    //}
 
-                    // Left
-                    if (x == 0)
-                    {
-                        faces.Add(Voxel.getLeftFace(new Vector3(x, y, z)));
-                    }
+                    //// Left
+                    //if (x == 0)
+                    //{
+                    //    faces.Add(Voxel.getLeftFace(new Vector3(x, y, z)));
+                    //}
 
-                    // Back
-                    if (z == chunkWidth - 1)
-                    {
-                        faces.Add(Voxel.getBackFace(new Vector3(x, y, z)));
-                    }
+                    //// Back
+                    //if (z == chunkWidth - 1)
+                    //{
+                    //    faces.Add(Voxel.getBackFace(new Vector3(x, y, z)));
+                    //}
 
                     // Front
                     if (z == 0)
@@ -125,9 +117,6 @@ public class BlockRenderTest : MonoBehaviour
         stopwatch.Stop();
 
         Debug.LogFormat("Mesh Build time ={0}", stopwatch.Elapsed);
-
-
-
     }
 
 
@@ -175,111 +164,8 @@ public class BlockRenderTest : MonoBehaviour
 
         Debug.LogFormat("Normal Recalc time ={0}", stopwatch.Elapsed);
 
-
-
- 
-
-
         meshFilter.mesh = mesh;
-
-
-
-
-
     }
-
-
-
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void UpdateMeshCube()
-    {
-        VoxelFace frontFaceData = Voxel.getFrontFace(new Vector3(0,0,0));
-        VoxelFace topFaceData = Voxel.getTopFace(new Vector3(0, 0, 0));
-
-
-        List<VoxelFace> faces = new List<VoxelFace>();
-
-
-        faces.Add(Voxel.getFrontFace(new Vector3(0, 0, 0)));
-        faces.Add(Voxel.getTopFace(new Vector3(0, 0, 0)));
-        faces.Add(Voxel.getLeftFace(new Vector3(0, 0, 0)));
-        faces.Add(Voxel.getRightFace(new Vector3(0, 0, 0)));
-        faces.Add(Voxel.getBottomFace(new Vector3(0, 0, 0)));
-        faces.Add(Voxel.getBackFace(new Vector3(0, 0, 0)));
-
-
-
-
-        // TODO:  might want to investigate turning this into arrays rather than lists
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
-        List<Vector3> normals = new List<Vector3>();
-
-        int faceCount = 0;
-        foreach( VoxelFace face in faces){
-
-            // copy the Vertex information
-            for(int i = 0; i < 4; i++)
-            {
-                vertices.Add(face.vertices[i]);
-                uvs.Add(face.uvs[i]);
-                normals.Add(face.normals[i]);
-            }
-
-            for (int i = 0; i<6; i++)
-            {
-                triangles.Add(face.triangles[i] + faceCount * 4);
-            }
-
-            faceCount++;
-        }
-
-
-
-
-
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.uv = uvs.ToArray();
-
-        mesh.normals = normals.ToArray();
-
-
-        //mesh.RecalculateNormals();
-
-
-
-        List<Vector3> calcnormals = new List<Vector3>();
-
-        mesh.GetNormals(calcnormals);
-
-        foreach(Vector3 normal in calcnormals)
-        {
-            Debug.Log(normal.x + " " + normal.y + " " + normal.z);
-
-
-
-
-        }
-        
-
-
-        Debug.Log(calcnormals[0].x + " " + calcnormals[0].y + " " + calcnormals[0].z);
-
-
-        meshFilter.mesh = mesh;
-
-        
-    }
-
 
 
 
